90152eabb9131df8b186be21362cd229528368c3
luongnguyen008 <53995452+luongnguyen008@users.noreply.github.com>
2020-05-01 13:08:07 +0700

    them adminAuth

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -10,9 +10,22 @@ module.exports.requireAuth = function (req, res, next){
   		if (err) throw err;
   		if(!result[0]){
   			res.redirect('/auth/login');
-  	}
+  		}
   		res.locals.user = result[0];
   		next()
 	});
-	
+};
+module.exports.adminAuth = function (req, res, next){
+	if(!req.signedCookies.userId){
+		res.redirect('/auth/login');
+		return;
+	}
+	con.query('SELECT * FROM users WHERE id = ?', req.signedCookies.userId, function (err, result) { 
+  		if (err) throw err;
+		if (result[0].username.localeCompare("admin")!== 0){
+			res.redirect('/products')
+	}
+	res.locals.user = result[0];
+	next()
+	});
 };
