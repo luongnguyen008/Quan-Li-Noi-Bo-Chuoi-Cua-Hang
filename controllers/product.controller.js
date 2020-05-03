90f06b2430af9631e62a2bfca2fb63c7a80378ba
luongnguyen008 <53995452+luongnguyen008@users.noreply.github.com>
2020-05-03 21:44:27 +0700

    add pagination and searchProduct

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,12 +2,39 @@ var mysql = require('mysql')
 var con = require('../mysql-connection')
 
 module.exports.index = function (req, res) {
+	var page = parseInt(req.query.page) || 1;
+	var perPage = 4;
+	var start = (page -1)* perPage;
+	var end = page*perPage;
+
 	var userId = req.signedCookies.userId;
 	con.query('SELECT * FROM users WHERE id = ?',userId, function (err, result) {
 		var storeId = result[0].storeId;
-    	con.query('SELECT * FROM products WHERE storeId = ?',storeId, function (err, result) { // retrieve data 
-    		if (err) throw err;
-    		res.render('./products/staffProducts', { products: result});
+		con.query('SELECT COUNT(id) AS NumberOfProducts FROM products WHERE storeId=?',storeId, function (err, result) {
+			var numberOfProducts= result[0].NumberOfProducts;
+			var numberOfPages= Math.ceil(numberOfProducts/perPage)
+			//console.log(numberOfPages);
+	    	con.query('SELECT * FROM products WHERE storeId = ?',storeId, function (err, result) {
+	    		if (err) throw err;
+	    		res.render('./products/staffProducts', { products: result.slice(start, end), page, numberOfPages});
+	    		});
   });
 });
+};
+
+module.exports.searchProducts = function (req, res) {
+  var id = req.params.storeId;
+  var q = req.query.q;
+  var userId = req.signedCookies.userId;
+	con.query('SELECT * FROM users WHERE id = ?',userId, function (err, result) {
+		var storeId = result[0].storeId;
+  		con.query('SELECT * FROM products WHERE storeId = ?', storeId, function (err, result) { // retrieve data 
+	    if (err) throw err;
+	    //console.log(result);
+	    var matchedProducts = result.filter(function(product){
+	      return product.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	    });
+	    res.render('./products/staffProducts', { products: matchedProducts});
+	  });
+  		});
 };
