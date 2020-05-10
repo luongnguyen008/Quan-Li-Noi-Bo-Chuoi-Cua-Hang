92b6cb1e4b02c4a22f83470eb6451d557b920124
luongnguyen008 <53995452+luongnguyen008@users.noreply.github.com>
2020-05-10 21:38:11 +0700

    maximize code

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql')
 var con = require('../mysql-connection')
+var Cart = require('../models/cart');
 
 module.exports.index = function (req, res) {
 	var page = parseInt(req.query.page) || 1;
@@ -7,9 +8,7 @@ module.exports.index = function (req, res) {
 	var start = (page -1)* perPage;
 	var end = page*perPage;
 
-	var userId = req.signedCookies.userId;
-	con.query('SELECT * FROM users WHERE id = ?',userId, function (err, result) {
-		var storeId = result[0].storeId;
+	var storeId = req.session.storeId;
 		con.query('SELECT COUNT(id) AS NumberOfProducts FROM products WHERE storeId=?',storeId, function (err, result) {
 			var numberOfProducts= result[0].NumberOfProducts;
 			var numberOfPages= Math.ceil(numberOfProducts/perPage)
@@ -17,24 +16,21 @@ module.exports.index = function (req, res) {
 	    	con.query('SELECT * FROM products WHERE storeId = ?',storeId, function (err, result) {
 	    		if (err) throw err;
 	    		res.render('./products/staffProducts', { products: result.slice(start, end), page, numberOfPages});
-	    		});
-  });
-});
+	    	});
+  		});
 };
 
 module.exports.searchProducts = function (req, res) {
-  var id = req.params.storeId;
-  var q = req.query.q;
-  var userId = req.signedCookies.userId;
-	con.query('SELECT * FROM users WHERE id = ?',userId, function (err, result) {
-		var storeId = result[0].storeId;
-  		con.query('SELECT * FROM products WHERE storeId = ?', storeId, function (err, result) { // retrieve data 
+	var id = req.params.storeId;
+	var q = req.query.q;
+	var storeId = req.session.storeId;
+	con.query('SELECT * FROM products WHERE storeId = ?', storeId, function (err, result) { // retrieve data 
 	    if (err) throw err;
 	    //console.log(result);
 	    var matchedProducts = result.filter(function(product){
 	      return product.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
 	    });
 	    res.render('./products/staffProducts', { products: matchedProducts});
-	  });
-  		});
+	});
 };
+
