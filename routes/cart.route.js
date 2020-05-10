34bfaffb98f2c6270fc174f292ba04ba0e041cf0
ngoc-trung <62040521+ngoc-trung@users.noreply.github.com>
2020-05-10 23:58:53 +0700

    add cart route

---
+++ routes/cart.route.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var router = express.Router()
+var controller = require('../controllers/cart.controller')
+
+router.get('/', controller.cartIndex)
+router.get('/add/:productId', controller.addToCart);
+router.get('/remove/:productId', controller.removeFromCart);
+router.get('/checkout', controller.checkOut);
+module.exports = router;
