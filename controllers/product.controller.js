710094907495e693b23ce39e8fc9741a57b38019
tungnb2611 <62040289+tungnb2611@users.noreply.github.com>
2020-05-11 21:46:06 +0700

    new product.comtroller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -4,7 +4,7 @@ var Cart = require('../models/cart');
 
 module.exports.index = function (req, res) {
 	var page = parseInt(req.query.page) || 1;
-	var perPage = 4;
+	var perPage = 6;
 	var start = (page -1)* perPage;
 	var end = page*perPage;
 
