e29187a77a8f3dbcba44ca9ea91ce3a94b7f4a7e
ngoc-trung <62040521+ngoc-trung@users.noreply.github.com>
2020-05-18 14:16:53 +0700

    add stats route

--- routes/store.route.js
+++ routes/store.route.js
@@ -1,12 +1,11 @@
-var express = require('express');
+var express = require('express')
 var multer = require('multer');
-var router = express.Router();
-var controller = require('../controllers/store.controller');
+var router = express.Router()
+var controller = require('../controllers/store.controller')
 var upload = multer({ dest: './public/uploads/' });
 
+//stores
 router.get('/', controller.index);
-
-router.get('/searchStores', controller.searchStores);
 
 router.get('/createStore', controller.createStore);
 
@@ -14,8 +13,14 @@ router.post('/createStore', controller.postCreateStore);
 
 router.get('/deleteStore/:storeId', controller.deleteStore);
 
+router.get('/:storeId/edit', controller.editStores);
+
+router.post('/:storeId/edit', controller.postEditStores);
+
 router.get('/:storeId', controller.viewStore);
 
+
+//users
 router.get('/:storeId/users', controller.storeUsers);
 
 router.get('/:storeId/users/search', controller.searchUsers);
@@ -28,6 +33,8 @@ router.post('/:storeId/users/edit/:userId', controller.postEditUsers);
 
 router.get('/:storeId/users/delete/:userId', controller.deleteUsers);
 
+
+//products
 router.get('/:storeId/products',upload.single('picture'), controller.storeProducts);
 
 router.get('/:storeId/products/createProduct', controller.createProduct);
@@ -40,11 +47,13 @@ router.get('/:storeId/products/editProduct/:id', controller.editProducts);
 
 router.post('/:storeId/products/editProduct/:id', upload.single('picture'), controller.postEditProducts);
 
-
-router.get('/:storeId/edit', controller.editStores);
-
-router.post('/:storeId/edit', controller.postEditStores);
-
 router.get('/:storeId/products/deleteProduct/:id', controller.deleteProducts);
+
+
+//statistics
+router.get('/:storeId/statistics', controller.viewStatisticsIndex);
+
+router.get('/:storeId/statistics/:from-:to', controller.viewResult);
+
 
 module.exports = router;
