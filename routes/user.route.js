8850e1beedd59f8ecd7262d66fe8fda5b781a012
unknown <thekhoa2206@gmail.com>
2020-05-25 20:11:01 +0700

    add calculate salary

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,9 +5,22 @@ var validate = require('../validate/user.validate');
 
 router.get('/', controller.index);
 
+router.get('/listUsers', controller.listUsers);
+
+router.get('/listSalary', controller.listSalary);
+
+router.get('/calculateSalary', controller.calculateSalary);
+
+router.post('/calculateSalary', controller.postCalculateSalary)
+
+router.get('/editSalary/:idSalary', controller.editSalary);
+
+router.post('/editSalary/:idSalary', controller.postEditSalary);
+
+router.get('/listSalary/searchSalary', controller.searchSalary);
+
 router.get('/create', controller.create);
 
 router.post('/create', validate.postCreate,controller.postCreate);
-
 
 module.exports = router;
