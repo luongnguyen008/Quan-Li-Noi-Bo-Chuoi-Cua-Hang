c6bd9a0300ab6ee2ff1f3ba82261bf3d5f623d7c
tungnb2611 <62040289+tungnb2611@users.noreply.github.com>
2020-05-25 22:18:03 +0700

    add new auth.rote.js

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,6 @@
-var express = require('express')
-var router = express.Router()
-var controller = require('../controllers/auth.controller')
+var express = require('express');
+var router = express.Router();
+var controller = require('../controllers/auth.controller');
 
 router.get('/login', controller.login);
 router.post('/login',controller.postLogin);
