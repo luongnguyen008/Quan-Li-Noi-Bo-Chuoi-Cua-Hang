458e3be91606fe61a49adb3a1c80d0f4436105ba
tungnb2611 <62040289+tungnb2611@users.noreply.github.com>
2020-05-25 22:20:01 +0700

    add new store.rote.js

--- routes/store.route.js
+++ routes/store.route.js
@@ -1,6 +1,6 @@
-var express = require('express')
+var express = require('express');
 var multer = require('multer');
-var router = express.Router()
+var router = express.Router();
 var controller = require('../controllers/store.controller')
 var upload = multer({ dest: './public/uploads/' });
 
